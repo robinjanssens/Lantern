56299509d2682f8937c4119264e7e890225e7199
Robin Janssens <robin@robinjanssens.net>
2018-06-08 14:28:16 +0200

    changed floating point variables and positions to bytes

--- position_generator.rb
+++ position_generator.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/ruby
 
-print "float positions[300][2] = { "
+print "const uint8_t positions[300][2] = { "
 for i in 0..300
   if i != 150 then
-    y = i * 10.1/12.0
+    y = i * 10.1/12.0         # y = 0 -> 252.5 mm
+    y = (y/252.5*255).round   # y = 0 -> 255
     x = i * 50.0/3.0 % 202.0  # x dir 0 -> 202 mm
-    output = "{" + y.round(3).to_s + "," + x.round(3).to_s + "}"
+    x = (x/202*255).round     # x = 0 -> 255
+    output = "{" + y.to_s + "," + x.to_s + "}"
     print output
     if i != 300 then
       print ", "
