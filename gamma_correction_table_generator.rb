6698894256f85862c09d55b7cc6eb76f2e3546f1
Robin Janssens <robin@robinjanssens.net>
2019-09-03 01:57:35 +0200

    added user input check to gamma correction generator

--- gamma_correction_table_generator.rb
+++ gamma_correction_table_generator.rb
@@ -1,7 +1,12 @@
 #!/usr/bin/ruby
 
-print "Gamma Correction Factor: "
-gamma = gets.chomp.to_f
+gamma = 0
+loop do
+  print "Gamma Correction Factor: "
+  gamma = gets.chomp.to_f
+break if gamma > 0
+  print "Gamma needs to be a real positive value!\n"
+end
 
 print "const uint8_t gamma_correction_table[256] = { "
 for i in 0..255
