ba48bc24fd1bb253e22898f471e6bd20ea5fbbfb
Robin Janssens <robin@robinjanssens.net>
2019-09-12 01:47:51 +0200

    switched to numPixels for led loops

--- src/Background.cpp
+++ src/Background.cpp
@@ -13,7 +13,7 @@ Background::Background(Adafruit_NeoPixel* strip, uint8_t* x_offset) {
 void Background::rainbow() {
   uint8_t value;
   uint8_t red, green, blue;
-  for (uint16_t led=0; led<300; led++) {
+  for (uint16_t led=0; led<strip->numPixels(); led++) {
     value = positions[led][1]+(*x_offset);  // 0 -> 255 (automatically modulo 256)
     red=rainbow_wave_table[value];
     green=rainbow_wave_table[(value+85)%256];
@@ -26,7 +26,7 @@ void Background::rainbow() {
 
 void Background::police() {
   uint8_t value, value2;
-  for (uint16_t led=0; led<300; led++) {
+  for (uint16_t led=0; led<strip->numPixels(); led++) {
     value = positions[led][1]+(*x_offset);  // 0 -> 255 (automatically modulo 256)
     if (value>0 && value<128) {
       value2 = 255-cosinus_table[2*value];
@@ -41,14 +41,14 @@ void Background::police() {
 
 void Background::fire() {
   uint8_t rand;
-  for (uint16_t led=0; led<300; led++) {
+  for (uint16_t led=0; led<strip->numPixels(); led++) {
     rand = random(10,100);
     strip->setPixelColor(led, strip->Color(rand, rand*0.75, 0));
   }
 }
 
 void Background::solid(uint8_t red, uint8_t green, uint8_t blue) {
-  for (uint16_t led=0; led<300; led++) {
+  for (uint16_t led=0; led<strip->numPixels(); led++) {
     strip->setPixelColor(led, strip->Color(red, green, blue));
   }
 }
@@ -60,11 +60,11 @@ void Background::solid(uint8_t red, uint8_t green, uint8_t blue) {
 void Background::flashes() {
   uint16_t led;
   if ( (counter % 4) == 0 ) {
-    led = random(300); // select random led
+    led = random(strip->numPixels()); // select random led
     flashes_overlay[led] = 255; // turn on
   }
   counter++; // automatically rolls over
-  for (led=0; led<300; led++) {
+  for (led=0; led<strip->numPixels(); led++) {
     uint8_t flash_intensity = flashes_overlay[led]/4;
     strip->setPixelColor(led, strip->Color(flash_intensity+getRed(strip->getPixelColor(led)), flash_intensity+getGreen(strip->getPixelColor(led)), flash_intensity+getBlue(strip->getPixelColor(led))));
     if ( flashes_overlay[led] > 0 ) { // fade away
